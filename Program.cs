89d9154f78486547a66776e2321b3d68c8e11734
Waked <komma336@gmail.com>
2017-11-16 00:39:50 +0100

    Working proj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TSST_router
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
