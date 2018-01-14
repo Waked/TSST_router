30c59191cf427bee59e41d71f66f8c768abc4874
Waked <komma336@gmail.com>
2018-01-14 01:40:16 +0100

    Add LRM to RC communication interface

---
+++ LRMRCCommunications/LinkStateUpdate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRMRCCommunications
+{
+    [Serializable]
+    public class LinkStateUpdate : Communications.Message
+    {
+        public Node beginNode;
+        public Node endNode;
+        public byte beginSNPP;
+        public byte endSNPP;
+        public int capacity;
+
+        public LinkStateUpdate(
+            string beginId,
+            string beginAS,
+            string beginSubnet,
+            byte beginSNPP,
+            string endId,
+            string endAS,
+            string endSubnet,
+            byte endSNPP,
+            int capacity)
+        {
+            beginNode = new Node(beginId, beginAS, beginSubnet);
+            endNode = new Node(endId, endAS, endSubnet);
+            this.beginSNPP = beginSNPP;
+            this.endSNPP = endSNPP;
+            this.capacity = capacity;
+        }
+    }
+}
---
+++ LRMRCCommunications/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRMRCCommunications
+{
+    /*
+     * Klasa opisujaca wezel sieciowy, tj. jego:
+     * - identyfikator urzadzenia
+     * - system autonomiczny
+     * - identyfikator podsieci
+     */
+    [Serializable]
+    public class Node
+    {
+        public string id;
+        public string asId;
+        public string snId;
+
+        public Node(string id, string asId, string snId)
+        {
+            this.id = id;
+            this.asId = asId;
+            this.snId = snId;
+        }
+    }
+}
